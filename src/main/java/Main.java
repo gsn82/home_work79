045da006220fc5ee2dcdf6f46475db5013f31305
Gorshkov Sergey <Gsn82@mail.ru>
2021-04-15 12:01:44 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM(1000);
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("Max", 300);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("John", 500);
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atm.withdraw("Nick", 400);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
